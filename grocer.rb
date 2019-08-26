4c0a57f50295831518dfb7abf5ed02ac57a7811a
Catherine Clause <cnclause123@gmail.com>
2019-08-26 02:55:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,36 @@
-def consolidate_cart(cart)
-  # code here
+def consolidate_cart(cart) 
+  final_hash = {} 
+  cart.each do |element_hash| 
+    element_hash.each do |item, attribute_hash| 
+      final_hash[item] ||= attribute_hash 
+      final_hash[item][:count] ? final_hash[item][:count] += 1 :   
+      final_hash[item][:count] = 1 
+  end 
+end 
+final_hash 
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+
+def apply_coupons(cart, coupons) 
+  coupons.each do |coupon|
+    item = coupon[:item]
+    if cart[item] && cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
+      cart["#{item} W/COUPON"] = {price: coupon[:cost]/coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]}
+      cart[item][:count] -= coupon[:num] 
+    elsif cart[item][:count] >= coupon[:num] && cart["#{item} W/COUPON"] 
+    cart["#{item} W/COUPON"][:count] += coupon[:num]  
+    cart[item][:count] -= coupon[:num] 
+    
+    end
+  end
+  cart
 end
+
 
 def apply_clearance(cart)
-  # code here
+  
 end
 
-def checkout(cart, coupons)
-  # code here
+def checkout(items, coupons)
+ 
 end
